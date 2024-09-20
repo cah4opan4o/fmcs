3ec5563a2657adeddf0cd27792c37b23673eb6a8
cah4opan4o <volodin.sasha2004@yandex.ru>
2024-09-21 00:10:50 +0700

    пункт2, добавлено отображение максимальной частоты в спектре сигнала

--- practice_1/task.py
+++ practice_1/task.py
@@ -12,14 +12,25 @@ t = np.arange(0, 1, 0.001)
 # Генерация сигнала
 y = A * np.sin(2 * np.pi * f * t + phi)
 
-
 # Преобразование Фурье
 Y = np.fft.fft(y)
 N = len(Y) # Количество точек
 Y_magnitude = np.abs(Y) / N
-frequencies = np.fft.fftfreq(N, d=0.001) 
+frequencies = np.fft.fftfreq(N, d=0.001)
+
+plt.figure()
+plt.subplot(2,1,1)
+plt.plot(t, y)
+plt.title('Непрерывный сигнал y(t) = 3sin(2πft + π/7)')
+plt.xlabel('Время (с)')
+plt.ylabel('Амплитуда')
+plt.grid()
+plt.axis('tight')
+plt.show()
+
 # Визуализация спектра
 plt.figure()
+plt.subplot(2,1,2)
 plt.plot(frequencies[:N // 2], Y_magnitude[:N // 2]) 
 plt.title('Спектр сигнала')
 plt.xlabel('Частота (Гц)')
