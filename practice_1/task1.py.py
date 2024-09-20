2c88319459ba89d04b1c4df03338be078ff4af92
cah4opan4o <volodin.sasha2004@yandex.ru>
2024-09-20 16:26:20 +0700

    Определить максимальную частоту в спектре данного сигнала

--- practice_1/task1.py.py
+++ practice_1/task1.py.py
@@ -12,18 +12,11 @@ t = np.arange(0, 1, 0.001)
 # Генерация сигнала
 y = A * np.sin(2 * np.pi * f * t + phi)
 
-
-# Преобразование Фурье
-Y = np.fft.fft(y)
-N = len(Y) # Количество точек
-Y_magnitude = np.abs(Y) / N
-frequencies = np.fft.fftfreq(N, d=0.001) 
-# Визуализация спектра
 plt.figure()
-plt.plot(frequencies[:N // 2], Y_magnitude[:N // 2]) 
-plt.title('Спектр сигнала')
-plt.xlabel('Частота (Гц)')
+plt.plot(t, y)
+plt.title('Непрерывный сигнал y(t) = 3sin(2πft + π/7)')
+plt.xlabel('Время (с)')
 plt.ylabel('Амплитуда')
 plt.grid()
-plt.xlim(0, 80)
+plt.axis('tight')
 plt.show()
