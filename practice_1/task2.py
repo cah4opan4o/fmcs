c2dcce8d1bea9aac305f6b43943e841a90c7a2b5
cah4opan4o <volodin.sasha2004@yandex.ru>
2024-09-21 11:42:00 +0700

    пункт 6: добавлено восстановление оригинального аналогового сигнала по массиву отсчётов

--- practice_1/task2.py
+++ practice_1/task2.py
@@ -1,4 +1,5 @@
 import numpy as np
+import matplotlib.pyplot as plt
 
 f = 40  # Частота в Гц
 A = 3   # Амплитуда
@@ -25,6 +26,10 @@ Frequency_bin_width = frequencies[1] - frequencies[0]
 memory_usage = y_samples.nbytes  # это в байтах
 memory_usage_kb = memory_usage / 1024  # переводим в КБ
 
+# Восстановление оригинального сигнала с помощью интерполяции
+t_fine = np.linspace(0, t_duration, 1000)  
+y_fine = A * np.sin(2 * np.pi * f * t_fine + phi)  
+
 # Вывод информации в консоль
 print("Дискретное преобразование Фурье (амплитуда):\n", np.abs(Y))
 print("Ширина спектра (максимальная частота):", max_frequency)
@@ -34,3 +39,16 @@ print("Объем памяти, занятый массивом y_samples:", mem
 with open('signal_data.txt', 'w') as f:
     for x, y in zip(t_samples, y_samples):
         f.write(f"{x:.6f} {y:.6f}\n")
+
+# Визуализация
+plt.figure(figsize=(12, 6))
+plt.plot(t_fine, y_fine, label='Оригинальный сигнал', color='blue')
+plt.plot(t_samples, y_samples, 'o-', label='Восстановленный сигнал (отсчеты)', color='orange')
+plt.title('Сравнение оригинального и восстановленного сигнала')
+plt.xlabel('Время (с)')
+plt.ylabel('Амплитуда')
+plt.legend()
+plt.grid()
+plt.xlim(0, t_duration)
+plt.ylim(-5, 5)
+plt.show()
